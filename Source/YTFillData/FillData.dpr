7c5b180c4b167782ab34b1972a025234389243be
dmzn <dmznlin@users.noreply.github.com>
2015-09-29 15:15:50 +0800

    +.添加云天数据填充程序,用于补全物料出厂报表数据.

---
+++ Source/YTFillData/FillData.dpr
@@ -0,0 +1,13 @@
+program FillData;
+
+uses
+  Forms,
+  UFormMain in 'UFormMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
